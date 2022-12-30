6f3003b5f105a9e312793916011a33126d299de0
DmitryyParkhomenko <dim.parkhom2007@gmail.com>
2022-12-30 18:01:08 +0100

    trying to fix form sending

--- src/mail.php
+++ src/mail.php
@@ -1,5 +1,6 @@
 <?php 
 
+
     $name = $_POST['name'];
     $subject = 'Запит на розрахунок';
     $email = $_POST['email'];
@@ -17,15 +18,18 @@
     $to = 'dim.parkhom2007@gmail.com';
     $from = trim($email);
     $message = 'Имя: ' . $name . "; \n" . 'Телефон: ' . $phone . "; \n" . "Email: $email";
-    $headers = 'From: ' . $from . "\r\n" . "Reply-To: $from" . "\r\n";
+    $headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
 
     if (mail($to, $subject, $message, $headers) == 1) {
         echo $message;
         mail($to, $subject, $message, $headers);
-        // header('Location: ../index.html');
+        header('Location: ../index.html');
     } else {
         header('Location: https://www.google.com');
     }
+
+
+
     // if ($name != '' && $phone != '' && $email != '') {
 
     //     $message = 'Имя: ' . $name . "\n" . 'Телефон: ' . $phone . "; \n" . "Email: $email";
